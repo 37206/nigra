7e0f0b72c5d4e02046c32673dfa632859e7cac78
Vlad <a37206@s65-201>
2012-08-20 14:58:54 +0400

    Cli.py

--- nigra/src/cli.py
+++ nigra/src/cli.py
@@ -1,6 +1,11 @@
-import urllib.request
 import sys
+import urllib.request
+import urllib.parse
 proxy_handler = urllib.request.ProxyHandler({'https':'127.0.0.1:3128','http': '127.0.0.1:3128'})
 opener = urllib.request.build_opener(proxy_handler)
-data=opener.open('https://www.google.ru')
-print(data.read())
+req=urllib.parse.urlencode({'email' :'','pass':''})
+req=req.encode(encoding='utf_8')
+data=opener.open('http://m.vk.com',req)
+data=opener.open('http://m.vk.com',data=None)
+print(data.getheaders())
+print(data.read().decode(encoding='utf_8'))
