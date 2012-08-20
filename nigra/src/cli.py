4310835ce40f399af6ba03bb1113f0839a2f92fd
37206 <37206@mail.ru>
2012-08-21 02:03:43 +0400

    waagh

--- nigra/src/cli.py
+++ nigra/src/cli.py
@@ -1,15 +1,16 @@
 import sys
 import urllib.request
 import urllib.parse
+import re
 proxy_handler = urllib.request.ProxyHandler({'https':'127.0.0.1:3128','http': '127.0.0.1:3128'})
 opener = urllib.request.build_opener(proxy_handler)
-site='http://login.vk.com?act=login'
+site='https://login.vk.com?act=login'
 post=urllib.parse.urlencode({'q':'1',
                              'al_frame':'1',
                              'from_host':'vk.com',
                              'act' : 'login',
-                             'email' :'',
-                             'pass':''})
+                             'email' :'a37206@gmail.com',#login
+                             'pass':'upyachka'})#pass
 post=post.encode(encoding='utf_8')
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13 (.NET CLR 3.5.30729)',
                    'Connection' : 'close',
@@ -21,16 +22,28 @@ req=urllib.request.Request(site,post,headers)
 data=urllib.request.urlopen(req)
 #data=opener.open('http://m.vk.com',data=None)
 html=data.read().decode(encoding='cp1251')
-start=html.index('''setCookieEx('sid',''')
-homeB=html.index('''parent.onLoginDone(\'''')
-homeE=html[homeB:].index(');')
-home=html[homeB+20:homeB+homeE-1]
 #print(html)
-cookie=html[start+18:start+80] #наша кука
-#print(cookie)
-req=urllib.request.Request('http://vk.com'+home,None,headers)
-req.add_header('Cookie','remixchk=5; '+cookie,
-               'remixsid='+cookie+';')
+home=re.search(r'''(?<=parent.onLoginDone\(\')/\w+''',html).group()#BLOOD FOR THE REGEX GOOOOD!11
+sid=re.search(r'''(?<='sid', ')\w+''',html).group()# OBEY REGEX!!!
+print(sid)
+
+
+
+post=urllib.parse.urlencode({'s':str(sid)})
+post=post.encode(encoding='utf_8')
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13',
+                   'Host' : 'vkontakte.ru',
+                   'Referer' : 'http://login.vk.com/?act=login',
+                   'Connection' : 'close',
+                   'Cookie' : 'remixchk=5; remixsid=nonenone',
+                   'Pragma' : 'no-cache',
+                   'Cache-Control' : 'no-cache'
+                  }
+#наши куки не для скуки
+site='http://vk.com?op=slogin'
+req=urllib.request.Request(site,post,headers)
 data=urllib.request.urlopen(req)
 html=data.read().decode('cp1251')
+#cookie=re.sub(r"expires=.+\sdomain=.+",'', str(data.info())) #ФАК ЙЕА REGEX!!!!1111111
+print(str(data.info()))
 print(html)
