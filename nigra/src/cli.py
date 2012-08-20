9f36cb2f272fbd899a4c9b47a7ede478064a1bc5
Vlad <a37206@s65-201>
2012-08-20 15:35:45 +0400

    1

--- nigra/src/cli.py
+++ nigra/src/cli.py
@@ -3,9 +3,17 @@ import urllib.request
 import urllib.parse
 proxy_handler = urllib.request.ProxyHandler({'https':'127.0.0.1:3128','http': '127.0.0.1:3128'})
 opener = urllib.request.build_opener(proxy_handler)
-req=urllib.parse.urlencode({'email' :'','pass':''})
-req=req.encode(encoding='utf_8')
-data=opener.open('http://m.vk.com',req)
-data=opener.open('http://m.vk.com',data=None)
+site='http://login.vk.com'
+post=urllib.parse.urlencode({'email' :'','pass':''})
+post=post.encode(encoding='utf_8')
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13 (.NET CLR 3.5.30729)',
+                   'Connection' : 'close',
+                   'Pragma' : 'no-cache',
+                   'Cache-Control' : 'no-cache',
+                  }
+req=urllib.request.Request(site,post,headers)
+data=opener.open(req)
+#data=opener.open('http://m.vk.com',data=None)
 print(data.getheaders())
+data=opener.open('http://m.vk.com',data=None)
 print(data.read().decode(encoding='utf_8'))
