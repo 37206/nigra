29f9195504fcb0dbe0582011a627bee1c980bfe4
Vlad <a37206@s65-201>
2012-08-22 07:30:37 +0400

    redone

--- nigra/src/cli.py
+++ nigra/src/cli.py
@@ -3,6 +3,7 @@ import urllib.request
 import urllib.parse
 import re
 import sqlite3
+import collections
 con = sqlite3.connect('1.db')
 c = con.cursor()
 # Create table
@@ -20,41 +21,32 @@ c.close()
 
 
 
-def VkAuth(proxy=None,login=None,password=None):
+def VkAuth(proxy=None, login=None, password=None):
     '''
-    proxy={protocol:address}
-        
+    Авторизация вконтактоты по хардкору без API
+    proxy={protocol:address} 
     
     '''
     class BaseAuthException(Exception): pass
     class LoginIsNone(BaseAuthException): pass
     class PasswordIsNone(BaseAuthException): pass
-#    class AuthFailed(Exception): pass 
     if proxy is not None: 
         proxy_handler = urllib.request.ProxyHandler(proxy)
         opener = urllib.request.build_opener(proxy_handler)
         urllib.request.install_opener(opener)
     site = 'https://login.vk.com?act=login'
     try:
-        for name in ('password','login'):
-            exec("if {0} is None:\n\traise {1}IsNone()".format(name,name.capitalize()))
-
-    
-    
-    
-#        if login is None:
-#            raise LoginIsNone()
-#        if password is None:
-#            raise PasswordIsNone()
+        for name in ('password', 'login'):
+            exec("if {0} is None:\n\traise {1}IsNone()".format(name, name.capitalize()))
     except BaseAuthException as err:
-        for name in ('Password','Login'):
-            exec("if isinstance(err,{0}IsNone):\n\tprint('VkAuth() Exception: {0} is empty')".format(name))
+        for name in ('Password', 'Login'):
+            exec("if isinstance(err,{0}IsNone):\n\tprint('VkAuth() Error: {0} is empty')".format(name))
         return None
     post = urllib.parse.urlencode({'q':'1',
                                  'al_frame':'1',
                                  'from_host':'vk.com',
                                  'act' : 'login',
-                                 'email' :login, 
+                                 'email' :login,
                                  'pass':password})
     post = post.encode(encoding='utf_8')
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13 (.NET CLR 3.5.30729)',
@@ -62,22 +54,17 @@ def VkAuth(proxy=None,login=None,password=None):
                        'Pragma' : 'no-cache',
                        'Cache-Control' : 'no-cache',
                       }
-    req = urllib.request.Request(site, post, headers)
-    
+    req = urllib.request.Request(site, post, headers)   
     data = urllib.request.urlopen(req)
-    #data=urllib.request.urlopen(req)
-    #data=opener.open('http://m.vk.com',data=None)
     html = data.read().decode(encoding='cp1251')
-    #print(html)
-    home = re.search(r'''(?<=parent.onLoginDone\(\')/\w+''', html).group() #BLOOD FOR THE REGEX GOOOOD!11
-    #sid=re.search(r'''(?<='sid', ')\w+''',html).group()# OBEY REGEX!!!
+    try:
+        home = re.search(r'''(?<=parent.onLoginDone\(\')/\w+''', html).group() #BLOOD FOR THE REGEX GOOOOD!11
+    except AttributeError:
+        print('VkAuth() Error: Authentication failed')
+        return None        
     cookie = re.search(r"remixsid=\w+;", str(data.info())).group() #ФАК ЙЕА REGEX!!!!1111111
-    if cookie is None:
-        print('nya!')
-        
-    post = urllib.parse.urlencode({'s':home})
+    post = urllib.parse.urlencode({'s':''})
     post = post.encode(encoding='utf_8')
-    #post=None
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13',
                        'Host' : 'vkontakte.ru',
                        'Referer' : 'http://login.vk.com/?act=login',
@@ -86,14 +73,82 @@ def VkAuth(proxy=None,login=None,password=None):
                        'Pragma' : 'no-cache',
                        'Cache-Control' : 'no-cache'
                       }
-    #наши куки не для скуки
-    site = 'http://vk.com'
-    req = urllib.request.Request(site, post, headers)
-    #data=urllib.request.urlopen(req)
-    data = opener.open(req)
-    html = data.read().decode('cp1251')
     return cookie
-proxy={'http':'127.0.0.1:3128','https':'127.0.0.1:3128'}
-login=(None,None)
-coo=VkAuth(proxy, *login)
+
+
+def group_search(keywords, cookie):
+    '''
+    парсер групп по ключевым словам, лол
+    '''
+    from html.parser import HTMLParser
+    parser = HTMLParser()
+    s = ''
+    for word in keywords:
+        s += word + ' '
+    print(s)
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13',
+                           'Host' : 'vkontakte.ru',
+                           'Referer' : 'http://vk.com/groups',
+                           'Connection' : 'close',
+                           'Cookie' : 'remixchk=5;' + cookie,
+                           'Pragma' : 'no-cache',
+                           'Cache-Control' : 'no-cache'
+                          }
+    
+    site = 'http://vk.com/al_groups.php'#поиск группы 
+    post = urllib.parse.urlencode({'act':'server_search', 'al':'1', 'q':s})#волшебный пост
+    post = post.encode(encoding='utf_8')
+    req = urllib.request.Request(site, post, headers)
+    data = urllib.request.urlopen(req)
+    
+    html = parser.unescape(data.read().decode('cp1251'))
+    html_pre = html.strip().splitlines()
+    groups = []
+    line = 'd'
+    group_stat=collections.namedtuple('group_stat', ['path','name','num'])
+    nstr=0
+    for line in html_pre:
+        line = line.lstrip()
+        if line.lstrip().startswith('<div class="group_row_labeled"><a href='):
+            #еще немного волшебства
+            temp1=re.search(r'(?<=<div class="group_row_labeled"><a href=")/\w+', line).group()
+            temp2=re.sub(r'<.+?>','', line)
+            nstr=1
+        elif nstr==1:
+            nstr=2
+        elif nstr==2:
+            print()
+            groups.append(group_stat(temp1,temp2,re.search(r'\d+',line).group()))
+            nstr=0
+    return groups
+
+
+
+
+proxy = {'http':'127.0.0.1:3128', 'https':'127.0.0.1:3128'}
+#proxy = None
+login = ('a37206@gmail.com', 'upyachka')
+coo = VkAuth(proxy, *login)
 print(coo)
+
+#
+##===================
+#headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13',
+#                       'Host' : 'vkontakte.ru',
+#                       'Referer' : 'http://vk.com/groups',
+#                       'Connection' : 'close',
+#                       'Cookie' : 'remixchk=5;' + coo,
+#                       'Pragma' : 'no-cache',
+#                       'Cache-Control' : 'no-cache'
+#                      }
+##========================
+#site = 'http://vk.com/feed'
+#post = urllib.parse.urlencode({'s':''})
+#post = post.encode(encoding='utf_8')
+#req = urllib.request.Request(site, post, headers)
+#data = urllib.request.urlopen(req)
+#html = data.read().decode('cp1251')
+
+
+found=group_search(['самые', 'милые', 'котятки'], coo)    
+print(found)
