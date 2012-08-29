5ed2f4a298d091492ca9909ac15cc201e674b8c7
Vlad <a37206@s65-201>
2012-08-29 15:32:46 +0400

    requests

--- nigra/src/cli.py
+++ nigra/src/cli.py
@@ -1,9 +1,8 @@
 import sys
-import urllib.request
-import urllib.parse
 import re
 import sqlite3
 import collections
+import requests
 con = sqlite3.connect('1.db')
 c = con.cursor()
 # Create table
@@ -21,7 +20,7 @@ c.close()
 
 
 
-def VkAuth(proxy=None, login=None, password=None):
+def VkAuth(login=None, password=None):
     '''
     Авторизация вконтактоты по хардкору без API
     proxy={protocol:address} 
@@ -30,10 +29,6 @@ def VkAuth(proxy=None, login=None, password=None):
     class BaseAuthException(Exception): pass
     class LoginIsNone(BaseAuthException): pass
     class PasswordIsNone(BaseAuthException): pass
-    if proxy is not None: 
-        proxy_handler = urllib.request.ProxyHandler(proxy)
-        opener = urllib.request.build_opener(proxy_handler)
-        urllib.request.install_opener(opener)
     site = 'https://login.vk.com?act=login'
     try:
         for name in ('password', 'login'):
@@ -42,20 +37,19 @@ def VkAuth(proxy=None, login=None, password=None):
         for name in ('Password', 'Login'):
             exec("if isinstance(err,{0}IsNone):\n\tprint('VkAuth() Error: {0} is empty')".format(name))
         return None
-    post = urllib.parse.urlencode({'q':'1',
-                                 'al_frame':'1',
-                                 'from_host':'vk.com',
-                                 'act' : 'login',
-                                 'email' :login,
-                                 'pass':password})
-    post = post.encode(encoding='utf_8')
+    post = {'q':'1', 'al_frame':'1', 'from_host':'vk.com', 'act' : 'login', 'email' :login, 'pass':password}
+#    post = requests.utils.(encoding='utf_8')
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13 (.NET CLR 3.5.30729)',
                        'Connection' : 'close',
                        'Pragma' : 'no-cache',
                        'Cache-Control' : 'no-cache',
                       }
-    req = urllib.request.Request(site, post, headers)   
-    data = urllib.request.urlopen(req)
+    #rewrite
+#    data = requests.post(site, params=post,proxies=proxy, headers=headers)   
+    data=requests.get('https://www.google.com:443',proxies=proxy)
+#    data = urllib.request.urlopen(req)
+    print(data.text)
+    sys.exit()
     html = data.read().decode(encoding='cp1251')
     try:
         home = re.search(r'''(?<=parent.onLoginDone\(\'/)\w+''', html).group() #BLOOD FOR THE REGEX GOOOOD!11
@@ -65,8 +59,8 @@ def VkAuth(proxy=None, login=None, password=None):
     cookie = re.search(r"remixsid=\w+;", str(data.info())).group() #ФАК ЙЕА REGEX!!!!1111111
     post = urllib.parse.urlencode({'s':''})
     post = post.encode(encoding='utf_8')
-    params=collections.namedtuple('params',['cookie', 'home'])
-    return params(cookie,home)
+    params = collections.namedtuple('params', ['cookie', 'home'])
+    return params(cookie, home)
 
 def group_search(keywords, cookie):
     '''
@@ -95,28 +89,27 @@ def group_search(keywords, cookie):
     html_pre = html.strip().splitlines()
     groups = []
     line = 'd'
-    group_stat=collections.namedtuple('group_stat', ['path','name','num'])
-    nstr=0
+    group_stat = collections.namedtuple('group_stat', ['path', 'name', 'num'])
+    nstr = 0
     for line in html_pre:
         line = line.lstrip()
         if line.lstrip().startswith('<div class="group_row_labeled"><a href='):
             #еще немного волшебства
-            temp1=re.search(r'(?<=<div class="group_row_labeled"><a href=")/\w+', line).group()
-            temp2=re.sub(r'<.+?>','', line)
-            nstr=1
-        elif nstr==1:
-            nstr=2
-        elif nstr==2:
+            temp1 = re.search(r'(?<=<div class="group_row_labeled"><a href=")/\w+', line).group()
+            temp2 = re.sub(r'<.+?>', '', line)
+            nstr = 1
+        elif nstr == 1:
+            nstr = 2
+        elif nstr == 2:
             print()
-            groups.append(group_stat(temp1,temp2,re.search(r'\d+',line).group()))
-            nstr=0
+            groups.append(group_stat(temp1, temp2, re.search(r'\d+', line).group()))
+            nstr = 0
     return groups
 
 
-def VkUpload(file,params):
+def VkUpload(file, params):
     '''грузилка изображений, например
     '''
-    import multipart
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13',
                        'Host' : 'vk.com',
                        'Referer' : 'http://vk.com/',
@@ -126,20 +119,20 @@ def VkUpload(file,params):
                        'Cache-Control' : 'no-cache'
                       }
     #post = urllib.parse.urlencode({'act':'server_search', 'al':'1', 'q':s})#волшебный пост
-    site='http://m.vk.com/album11888818_161787398?act=add&from=select'
+    site = 'http://m.vk.com/album11888818_161787398?act=add&from=select'
     post = urllib.parse.urlencode({'s':''}).encode(encoding='utf_8')
     req = urllib.request.Request(site, post, headers)
     data = urllib.request.urlopen(req)  
     html = data.read().decode('cp1251')
-    site=re.search(r'''(?<=<form action=")[^"]+''',html).group() #наш урл для загрузки фотачекк, мяффф
+    site = re.search(r'''(?<=<form action=")[^"]+''', html).group() #наш урл для загрузки фотачекк, мяффф
     print(params.home.lstrip('id'))
-    site='http://vk.com/al_wall.php'
+    site = 'http://vk.com/al_wall.php'
     post = urllib.parse.urlencode({'act': 'post',
       'type': 'photos_upload',
       'to_id': params.home.strip('id'),
       'attach1_type': 'photos_list',
       'attach1': open(file, 'rb'),
-      'hash': hash} )
+      'hash': hash})
     post = post.encode(encoding='utf_8')
     req = urllib.request.Request(site, post, headers)
     data = urllib.request.urlopen(req)  
@@ -151,12 +144,16 @@ def VkUpload(file,params):
 
 
 
-proxy = {'http':'127.0.0.1:3128', 'https':'127.0.0.1:3128'}
+proxy = {'http':'127.0.0.1:3128','https':'127.0.0.1:3128'}
 #proxy = None
-login = ('', '')
-params = VkAuth(proxy, *login)
+#if proxy is not None: 
+#    proxy_handler = urllib.request.ProxyHandler(proxy)
+#    opener = urllib.request.build_opener(proxy_handler)
+#    urllib.request.install_opener(opener)
+login = ('a37206@gmail.com', 'upyachka')
+params = VkAuth(*login)
 print(params)
-file='./1.jpg'
+file = './1.jpg'
 #VkUpload(file,coo)
 
 #
@@ -181,5 +178,5 @@ file='./1.jpg'
 
 
 
-found=group_search(['самые', 'котятки', 'милые'], params)    
+found = group_search(['самые', 'котятки', 'милые'], params)    
 VkUpload(file, params)
